f9bd827edfe036487cb403967a509216002780b3
ziap <67962871+ziap@users.noreply.github.com>
2022-01-06 20:14:14 +0700

    Improve wrapper

--- Wrapper.cs
+++ Wrapper.cs
@@ -8,33 +8,37 @@ class Wraper : System.Windows.Forms.Form
         // Initial form configuration
         Width = 800;
         Height = 600;
-        Text = "Loading...";
+        Text = Application.ProductName;
         StartPosition = FormStartPosition.CenterScreen;
         InitWebView();
     }
-    
+
     async void InitWebView() {
+        // Create the WebView
         var webView = new WebView2() {Dock = DockStyle.Fill};
         var env = await CoreWebView2Environment.CreateAsync(null, Path.Combine(Path.GetTempPath(), Application.ProductName), null);
         await webView.EnsureCoreWebView2Async(env);
         webView.CoreWebView2.SetVirtualHostNameToFolderMapping("0.0.0.0", Path.Combine(Application.StartupPath, "wwwroot"), CoreWebView2HostResourceAccessKind.Allow);
+
+        // Listen to some events
+        webView.NavigationCompleted += (sender, e) => webView.Focus();
         webView.CoreWebView2.DocumentTitleChanged += (sender, e) => Text = webView.CoreWebView2.DocumentTitle;
+
+        // Use window.close() in JavaScript to close the application
         webView.CoreWebView2.WindowCloseRequested += (sender, e) => Application.Exit();
 #if DEBUG
-        // Change this to your web server
+        // Change this to your dev server
         webView.Source = new Uri("https://0.0.0.0/index.html");
 #else
         webView.Source = new Uri("https://0.0.0.0/index.html");
+
+        // Production settings
         webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
         webView.CoreWebView2.Settings.IsStatusBarEnabled = false;
 #endif
         Controls.Add(webView);
     }
 
-
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
